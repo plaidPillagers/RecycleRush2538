c7424098e1d3f1d71d797fb6745bf6f301adeb20
plaidPillagers <lscott@plaidpillagers.com>
2015-01-28 18:09:32 -0600

    added wiring table and modified actuators

--- src/org/usfirst/frc2538/RecycleRush2538/commands/containerConfig.java
+++ src/org/usfirst/frc2538/RecycleRush2538/commands/containerConfig.java
@@ -34,11 +34,12 @@ public class  containerConfig extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.intake.setConfig();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
